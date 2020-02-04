05aff42fb3ae667c6a06b57d2dd9f1d61d04d6b5
aleex-adal <sopranalto@gmail.com>
2020-02-04 16:46:38 -0500

    figuring out types

--- ex1/ex1file.go
+++ ex1/ex1file.go
@@ -1,5 +1,10 @@
 package ex1
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 // ReverseRunes returns its argument string reversed rune-wise left to right.
 func ReverseRunes(s string) string {
 	r := []rune(s)
@@ -7,4 +12,40 @@ func ReverseRunes(s string) string {
 		r[i], r[j] = r[j], r[i]
 	}
 	return string(r)
+}
+
+// ChanTest tests channel functionality
+func ChanTest() {
+	type Packet struct {
+		Msg    string
+		Status int
+	}
+
+	messages := make(chan Packet)
+
+	go func() {
+		messages <- Packet{
+			Msg:    "begin",
+			Status: 200,
+		}
+
+		received := <-messages
+		out, err := json.Marshal(received)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("worker received: " + string(out))
+	}()
+
+	received := <-messages
+	out, err := json.Marshal(received)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%T\n", out)
+	fmt.Printf("%T\n", string(out))
+	fmt.Printf("%s\n", string(out))
+
 }
--- go-explore.go
+++ go-explore.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/aleex-adal/go-explore/ex1"
 )
 
 func main() {
 	fmt.Printf("Hello world! exploration\n")
 	fmt.Println(ex1.ReverseRunes("!oG ,olleH"))
+
+	ex1.ChanTest()
 }
